bba44b94148bd412d5a1e07ea37db81dea12282a
artyomromaniv <romaniv.artyom8@gmail.com>
2022-12-01 13:07:07 +0300

    add componentDidMount и поместил туда запрос (54 выпуск)

--- src/components/Users/UsersС.tsx
+++ src/components/Users/UsersС.tsx
@@ -8,9 +8,11 @@ import {UsersType} from "../../redux/usersReducer";
 
 export class UsersC extends React.Component<MainUsersContainerType, UsersType> {
 
-    constructor(props:MainUsersContainerType) {
+    constructor(props: MainUsersContainerType) {
         super(props);
+    }
 
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
